8d1427ebda306050275ac8b861a539f4291eb72e
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-11-17 18:11:01 +0100

    Enhance start.sh to run frontend and backend concurrently and load .env file

--- start.sh
+++ start.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+set -e
 
 concurrently \
-  "cd helios-backend && ./gradlew bootRun" \
-  "cd helios-frontend && npm run dev"
+  --names "BACKEND,FRONTEND" \
+  --prefix "[HELIOS {name}]" \
+  --prefix-colors "yellow,cyan" \
+  "
+    cd helios-backend && \
+    if [ -f .env ]; then \
+      set -o allexport &&
+      source .env &&
+      set +o allexport; \
+    else \
+      echo '.env file not found' && exit 1; \
+    fi && \
+    ./gradlew bootRun
+  " \
+  "
+    cd helios-frontend && \
+    npm run dev
+  "
