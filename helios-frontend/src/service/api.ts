8a447341a752aebbcca2c92ae0a16db9d934e2ce
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-12-16 14:38:14 +0100

    Refactor API functions into a class structure for better organization and maintainability

--- helios-frontend/src/service/api.ts
+++ helios-frontend/src/service/api.ts
@@ -1,28 +1,30 @@
 import axios from 'axios';
 import type { Collectible } from '../types/Collectible';
 
-const url = 'http://localhost:8080/api'
-
-export function fetchCollectibles(token: string): Promise<Collectible[]> {
-    return axios.get(`${url}/collectibles/data`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    }).then(res => res.data);
-}
-
-export function fetchCollectiblesByType(token: string, type: string): Promise<Collectible[]> {
-    return axios.get(`${url}/collectibles/data/${type}`, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    }).then(res => res.data);
-}
-
-export function updateCollectibles(token: string, collectibles: boolean[]): Promise<void> {
-    return axios.post(`${url}/collectibles/data`, { collectibles }, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    }).then(() => {});
+export class Api {
+    static url = 'http://localhost:8080/api';
+    
+    async fetchCollectibles(token: string): Promise<Collectible[]> {
+        return axios.get(`${Api.url}/collectibles/data`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then(res => res.data);
+    }
+    
+    async fetchCollectiblesByType(token: string, type: string): Promise<Collectible[]> {
+        return axios.get(`${Api.url}/collectibles/data/${type}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then(res => res.data);
+    }
+    
+    async updateCollectibles(token: string, collectibles: boolean[]): Promise<void> {
+        return axios.post(`${Api.url}/collectibles/data`, { collectibles }, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then(() => {});
+    }
 }
