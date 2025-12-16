2a64290654bef0d45a3a0b41116732e911b6974b
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-12-16 14:32:29 +0100

    Mirror the backend methods to the frontend

--- helios-frontend/src/service/api.ts
+++ helios-frontend/src/service/api.ts
@@ -3,6 +3,26 @@ import type { Collectible } from '../types/Collectible';
 
 const url = 'http://localhost:8080/api'
 
-export function fetchCollectibles(): Promise<Collectible[]> {
-    return axios.get(`${url}/collectibles/data`).then(res => res.data);
+export function fetchCollectibles(token: string): Promise<Collectible[]> {
+    return axios.get(`${url}/collectibles/data`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    }).then(res => res.data);
+}
+
+export function fetchCollectiblesByType(token: string, type: string): Promise<Collectible[]> {
+    return axios.get(`${url}/collectibles/data/${type}`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    }).then(res => res.data);
+}
+
+export function updateCollectibles(token: string, collectibles: boolean[]): Promise<void> {
+    return axios.post(`${url}/collectibles/data`, { collectibles }, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    }).then(() => {});
 }
